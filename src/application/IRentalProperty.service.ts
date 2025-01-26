dc47eaf757423d0dcea92beaa3c3e3c2554a1cdf
geboerswout <wout.geboers@student.ucll.be>
2025-01-26 17:00:09 +0100

    simplified interface

--- src/application/IRentalProperty.service.ts
+++ src/application/IRentalProperty.service.ts
@@ -1,10 +1,7 @@
 import { RentalProperty } from "@domain/rentalProperty.ts";
 
-export interface RentalPropertyReadService {
+export interface RentalPropertyService {
     getAll(): RentalProperty[];
     getById(id: string): RentalProperty;
-}
-
-export interface RentalPropertyCommandService {
     create(rentalProperty: RentalProperty): RentalProperty;
 }
