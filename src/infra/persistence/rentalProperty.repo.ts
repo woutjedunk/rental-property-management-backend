193f7f2778976084688c1ece2515c853eb3076ae
geboerswout <wout.geboers@student.ucll.be>
2025-02-19 12:56:18 +0100

    implement RentalPropertyRepoPrisma with NOT_IMPLEMENTED methods

--- src/infra/persistence/rentalProperty.repo.ts
+++ src/infra/persistence/rentalProperty.repo.ts
@@ -1,7 +1,9 @@
 
 import { RentalProperty } from "@domain/rentalProperty.ts";
 import { IRentalPropertyRepo } from "@application/IRentalPropertyRepo.ts";
- 
+import { ApplicationError } from "../../shared/errors/ApplicationError.ts";
+import { applicationErrorResponses, httpStatusCodes } from "../../shared/applicationResponses.ts";
+import { PrismaClient } from "@prisma/client";
 
 class RentalPropertyRepo implements IRentalPropertyRepo {
 
@@ -18,6 +20,27 @@ class RentalPropertyRepo implements IRentalPropertyRepo {
     save(rentalProperty: RentalProperty): RentalProperty {
         this.rentalProperties.push(rentalProperty);
         return rentalProperty;
+    }
+}
+
+class RentalPropertyRepoPrisma implements IRentalPropertyRepo {
+    
+    private client: PrismaClient;
+
+    constructor(injectedClient: PrismaClient) {
+        this.client = injectedClient;
+    }
+
+    getAll(): RentalProperty[] {
+        throw new ApplicationError(applicationErrorResponses.NOT_IMPLEMENTED, httpStatusCodes.NOT_IMPLEMENTED);
+    }
+
+    getById(id: string): RentalProperty | undefined {
+        throw new ApplicationError(applicationErrorResponses.NOT_IMPLEMENTED, httpStatusCodes.NOT_IMPLEMENTED);
+    }
+
+    save(rentalProperty: RentalProperty): RentalProperty {
+        throw new ApplicationError(applicationErrorResponses.NOT_IMPLEMENTED, httpStatusCodes.NOT_IMPLEMENTED);
     }
 }
 
