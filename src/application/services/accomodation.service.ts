7fd580d21dd80b575c96263e45ce55a0b2d34ee2
geboerswout <wout.geboers@student.ucll.be>
2025-02-19 20:44:36 +0100

    refactor service

--- src/application/services/accomodation.service.ts
+++ src/application/services/accomodation.service.ts
@@ -1,6 +1,18 @@
 import { IAccomodation } from "@domain/accomodation/IAccomodation.ts";
+import { RentalDomainAccomodation } from "@domain/accomodation/RentalDomainAccomodation.ts";
+import { Address } from "@domain/address/address.ts";
+import { randomUUID } from "node:crypto";
+import { RentalDomain } from "@domain/rentalDomain/RentalDomain.ts";
+import { StandaloneAccommodation } from "@domain/accomodation/StandaloneAccomodation.ts";
 
 
-const createAccomodation = (accomodation: IAccomodation) => {
-    
+
+function CreateAccomodation(accomodation: IAccomodation) {
+    if (accomodation instanceof RentalDomainAccomodation) {
+        // persist
+    } else if (accomodation instanceof StandaloneAccommodation) {
+        // persist
+    }
+
+    throw new Error("Invalid accomodation type");
 }
